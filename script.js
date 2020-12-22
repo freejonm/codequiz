ffcdf436110433374b3f7ce7fa99832daec12358
Jon Free <freejonm@gmail.com>
2020-12-22 15:42:57 -0500

    fixing scorekeeping and seconds demerits on incorrect answers

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 var timeEl = document.querySelector(".time");
-
-
-
 var startButton = document.querySelector("#start-btn");
-
 var questionContainer = document.querySelector("#question-container");
-
 var question = document.querySelector("#question");
-
 var option1 = document.querySelector("#option1");
-
 var option2 = document.querySelector("#option2");
-
 var answerClick = document.querySelector(".answer-buttons")
-
 var next = document.querySelector("#next-btn");
+
+var secondsLeft = 10;
+var secondsPenalty = 5;
+
+var i = 0;
+var score = 0;
+
 
 startButton.addEventListener("click", runQuiz);
 
 // Timer
-var secondsLeft = 10;
-var secondsPenalty = 5;
+
 
 // function setTime() {
 //   var timerInterval = setInterval(function() {
@@ -42,8 +39,6 @@ var secondsPenalty = 5;
 //   setTime();
 
 
-var i = 0;
-var score = 0;
 
 function runQuiz(){
     question.textContent = questions[i].question;
@@ -59,7 +54,8 @@ function runQuiz(){
         
         else{
             alert("you're wrong!");
-            
+            secondsLeft = secondsLeft - secondsPenalty;
+    
         }
     console.log(score);
     console.log(secondsLeft);
