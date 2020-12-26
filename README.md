0db85d5d3c5ab14136b54a9404da831c89fdf15c
Jon Free <freejonm@gmail.com>
2020-12-26 10:23:32 -0500

    working on readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# A basic quiz application
+This is a simple quiz application that begins with a start page:
+
+![screenshot of the quiz startpage](./assets/cqstartpage)
--- scores.js
+++ scores.js
@@ -16,23 +16,3 @@ clear.addEventListener("click", function(){
     highscores.removeChild(highscores.lastElementChild);
   }
 })
-// sort highscores by score property in descending order
-
-// highscores.sort(function(a, b) {
-//   return b.score - a.score;
-// });
-
-// highscores.forEach(function(score) {
-//   // create li tag for each high score
-//   var liTag = document.createElement("li");
-//   liTag.textContent = score.initials + " - " + score.score;
-
-//   // display on page
-//   var olEl = document.getElementById("user");
-//   olEl.appendChild(liTag);
-// });
-
-// document.getElementById("clear").addEventListener("click", function() {
-//   window.localStorage.removeItem("user");
-//   window.location.reload();
-// });
